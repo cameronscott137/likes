6eb467ac98b40e19aa628230cd1c5352ab2f5bb0
cameronscott137 <cscott@inquiri.com>
2020-06-09 09:37:14 -0400

    Order likes by the date the tweet was created.

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -11,10 +11,9 @@ class LikesController extends Controller
     public function __invoke(Request $request)
     {
         $likes = Like::whereSearch($request->term)
+            ->orderBy('created_at', 'desc')
             ->offset($request->offset)
             ->limit(40);
-
-        Log::info($request->offset);
 
         if ($request->expectsJson()) {
             return response($likes->get(), 200);
